50395f7769f270e88d618d137957fdd2beb10d7d
A-Kakko <kkbjdynasty@gmail.com>
2025-11-19 22:00:00 +0900

    リストを足してみたけど、反映されず

--- src/app.rs
+++ src/app.rs
@@ -1,27 +1,26 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+pub struct MemoApp {
     textbox: String,
-
+    selected: usize,
+    items: Vec<String>,
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
 }
 
-impl Default for TemplateApp {
+impl Default for MemoApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            textbox: "test".to_owned(),
+            textbox: String::new(),
+            selected: 0,
+            items: vec!["test1".to_string(), "test2".to_string()],
             value: 2.7,
         }
     }
 }
 
-impl TemplateApp {
+impl MemoApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -37,7 +36,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for MemoApp {
     /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -69,17 +68,14 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+            egui::ScrollArea::vertical()
+                .max_height(400.0)
+                .show(ui, |ui| {
+                    for (i, item) in self.items.iter().enumerate() {
+                        ui.selectable_value(&mut self.selected, i, item);
+                    }
+                });
 
             ui.separator();
 
